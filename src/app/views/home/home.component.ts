72043a10d66ffcb92d1de3a7da8596b443743d0f
Hebert Paziam <hebert.paziam@outlook.com>
2025-07-02 17:52:56 -0300

    feat: add X_API_KEY to HomeComponent and update environment configurations

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '@environment';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-
+  X_API_KEY = environment.X_API_KEY;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
+  API_URL: 'https://qugkx0grkb.execute-api.sa-east-1.amazonaws.com/api',
+  X_API_KEY: process?.env?.['X_API_KEY'] || 'X_API_KEY',
   TITLE_PREFIX: 'Challenge Exchange Order',
 };
